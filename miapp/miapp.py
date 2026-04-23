b113fd63afa3a095ef6f81fc9034b9d7978e1f9e
Alejandro A Crug <alexalcru@gmail.com>
2026-04-23 17:19:08 +0200

    test linter 3

---
+++ miapp/miapp.py
@@ -0,0 +1,11 @@
+""" Docstring del módulo """
+
+
+def suma(a: int, b: int) -> int:
+    """ docstring function
+    # ya no 
+    # debe dar errores en linter
+    """
+    x = "no se usa"
+    return a + b
+   
